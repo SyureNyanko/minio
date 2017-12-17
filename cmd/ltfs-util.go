253bd6c5b15665babb0ecb01358e91f2aa738bea
SyureNyanko <dr.myself+github@gmail.com>
2017-12-17 17:16:06 +0900

    Lock for migrating function

--- cmd/ltfs-util.go
+++ cmd/ltfs-util.go
@@ -38,14 +38,3 @@ func (m *FileLock) Unlock() error {
 	return nil
 }
 
-/*
-func main() {
-	l := NewFileLock("main.go")
-	fmt.Println("try  locking...")
-	l.Lock()
-	fmt.Println("locked!")
-	time.Sleep(10 * time.Second)
-	l.Unlock()
-	fmt.Println("unlock")
-}
-*/
